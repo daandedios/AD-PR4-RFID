30d0320bf6af0249ddea13d0907c77ef905b2a73
Daan de Dios <dedios.daan@gmail.com>
2019-03-21 15:03:22 +0100

    🎉 initial commit - add file, add base structure✏️

---
+++ AD-PR4-RFID.cpp
@@ -0,0 +1,21 @@
+
+//Developer:  Daan de Dios
+//Date:       2019-03-21
+//function:   RFID
+
+
+//----------------------global vars---------------------------------
+
+
+
+
+//setup runs once, when the Arduino is first oiwerd on.
+void setup() {
+  
+}
+
+//loop runs continuously after the setup() has completed.
+void loop() {
+
+
+}
