68bf93e2f30ed24d7d387083d048fc6728b7bfd4
Daan de Dios <dedios.daan@gmail.com>
2019-03-21 15:15:31 +0100

    add UID-READ file+📝 - add basic structure

---
+++ UID-READ.cpp
@@ -0,0 +1,24 @@
+
+//Developer:  Daan de Dios
+//Date:       2019-03-21
+//function:   UID - READ
+
+//##   this code is responsible for reading the UID of the RFID tags
+
+
+
+//----------------------global vars---------------------------------
+
+
+
+
+//setup runs once, when the Arduino is first oiwerd on.
+void setup() {
+  
+}
+
+//loop runs continuously after the setup() has completed.
+void loop() {
+
+
+}
