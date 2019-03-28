f26c7c76cc3380ba6ff0336833e1928d66b9f882
Daan de Dios <dedios.daan@gmail.com>
2019-03-28 14:55:33 +0100

    chnage

--- UID-READ.cpp
+++ UID-READ.cpp
@@ -1,35 +1,43 @@
-
-//Developer:  Daan de Dios
-//Date:       2019-03-21
-//function:   UID - READ
-
-//##   this code is responsible for reading the UID of the RFID tags
-
-
-
-//----------includes------------------------------------------------
-// SPI library (Serial Peripheral Interface (SPI))
 #include <SPI.h>
-
-// Arduino RFID Library
 #include <MFRC522.h>
 
+#define RST_PIN         5           // Configurable, see typical pin layout above
+#define SS_PIN          10          // Configurable, see typical pin layout above
 
-//----------------------global vars and defines---------------------
-// Reset Pin for SPI interface
-#define RST_PIN   9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 
-//Slave select pin
-#define SS_PIN    10
-
-
-//setup runs once, when the Arduino is first oiwerd on.
+//*****************************************************************************************//
 void setup() {
-  
+  Serial.begin(9600);                                           // Initialize serial communications with the PC
+  SPI.begin();                                                  // Init SPI bus
+  mfrc522.PCD_Init();                                              // Init MFRC522 card
+  Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read
 }
 
-//loop runs continuously after the setup() has completed.
+//*****************************************************************************************//
 void loop() {
 
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
 
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.println(F("**Card Detected:**"));
+
+
+  mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump some details about the card
+
+
+
+  //----------------------------------------
+
+  Serial.println(F("\n**End Reading**\n"));
+
+ 
 }
+//*****************************************************************************************//
