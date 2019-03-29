aa6f0913331f8a9acde0ee7fec3a685f6a49549f
Daan de Dios <dedios.daan@gmail.com>
2019-03-29 17:16:18 +0100

    add Display modul to source file 🎉

--- source/source.cpp
+++ source/source.cpp
@@ -12,6 +12,9 @@
 //servo lib.
 #include <Servo.h>
 
+//display lib.
+#include <LiquidCrystal.h>
+
 
 //---------------------- includs --------------------------------
 
@@ -27,8 +30,24 @@ Servo servoBarrier;  // create servo object to control the barrier Servo.
 //Servo positions
 int pos_servoBarrierOpen = 65;
 int pos_servoBarrierClose = 0;
-
 //=========================== servo end =========================
+
+
+//=========================== Display  ==========================
+const int rs = 9, // 
+          en = 8, 
+          d4 = 4, 
+          d5 = 3, 
+          d6 = 6, 
+          d7 = 2;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int numberOfFreeParkingSpaces = 10;
+//=========================== Display end =======================
+
+
+
 
 
 //---------------------- global vars end ------------------------
@@ -54,6 +73,17 @@ void servo() {
 //=========================== servo end =========================
 
 
+//=========================== Display  ==========================
+void setupDisplay() {
+  lcd.begin(16,2);
+}
+//=========================== Display end =======================
+
+
+
+
+
+
 //----------------------- functions end -------------------------
 
 
@@ -66,26 +96,41 @@ void servo() {
 //setup runs once, when the Arduino is first oiwerd on.
 void setup() {
 
+  //=========================== servo =============================
+  servoBarrier.attach(7);  // attaches the servo on pin 7 to the servo object (servoBarrier)
+  servoBarrier.write(0);   // initialize servo stage 0 --> close Barrier
+  //=========================== servo end =========================
 
-//=========================== servo =============================
-myservo.attach(7);  // attaches the servo on pin 7 to the servo object (servoBarrier)
-myservo.write(0);   // initialize servo stage 0 --> close Barrier
-//=========================== servo end =========================
+
+  //=========================== Display  ==========================
+  setupDisplay();
+  //=========================== Display end =======================
 
   
 }
 
+
+
+
+
+
+
+
 //loop runs continuously after the setup() has completed.
 void loop() {
 
-//=========================== servo =============================
+  //=========================== servo =============================
+  servo(); // opens barrier and clos barrier after 1000 miliseconds.
+  //=========================== servo end =========================
 
 
-servo(); // opens barrier and clos barrier after 1000 miliseconds.
+  //=========================== Display  ==========================
+  lcd.setCursor(0, 1);
+  lcd.print("Anzahl Freie Parkplätze: ");
+  lcd.print(numberOfFreeParkingSpaces);
+  //=========================== Display end =======================
 
 
-
-//=========================== servo end =========================
 
 
 }
