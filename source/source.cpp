b3f9bd3aefc74b02eb20c347b0a610de0af182a8
Daan de Dios <dedios.daan@gmail.com>
2019-04-02 23:25:32 +0200

    add loop block to loop function

--- source/source.cpp
+++ source/source.cpp
@@ -157,6 +157,48 @@ void loop() {
   //=========================== Display end =======================
 
   //=========================== RFID ==============================
+   // Diese Funktion wird in Endlosschleife ausgeführt
+ 
+  // Nur wenn eine Karte gefunden wird und gelesen werden konnte, wird der Inhalt von IF ausgeführt
+  // PICC = proximity integrated circuit card = kontaktlose Chipkarte
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial() ) {
+    Serial.print("Gelesene UID:");
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      // Abstand zwischen HEX-Zahlen und führende Null bei Byte < 16
+      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      Serial.print(mfrc522.uid.uidByte[i], HEX);
+    } 
+    Serial.println();
+ 
+    // UID Vergleichen mit card_uid
+    card_check = true;
+    for (int j=0; j<4; j++) {
+      if (mfrc522.uid.uidByte[j] != card_uid[j]) {
+        card_check = false;
+      }
+    }
+ 
+    // UID Vergleichen mit batch_uid
+    batch_check = true;
+    for (int j=0; j<4; j++) {
+      if (mfrc522.uid.uidByte[j] != batch_uid[j]) {
+        batch_check = false;
+      }
+    }
+ 
+    if (card_check) {
+     Serial.print("card");
+    }
+ 
+    if (batch_check) {
+     Serial.print("batch");
+    }
+ 
+    // Versetzt die gelesene Karte in einen Ruhemodus, um nach anderen Karten suchen zu können.
+    mfrc522.PICC_HaltA();
+    delay(1000);
+
+  }
   //=========================== RFID end ==========================
 
 
